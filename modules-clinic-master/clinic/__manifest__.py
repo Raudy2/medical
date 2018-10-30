521b5f77b133a58e610daa6b3ddd371e0bc430bd
Raudy beltre segura <maxbeltre@gmail.com>
2018-10-30 10:59:57 -0400

    medical module

---
+++ modules-clinic-master/clinic/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "clinic",
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Module management clinic
+    """,
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+}
